0a0cc2d12a197a094105c15b783fe2e0ab9a1aa2
Irina K. Tezaur <ikalash@sandia.gov>
2020-02-02 21:53:52 -0700

    Updating albany branch of spack in SNLComputation to work with current version of Albany.

--- var/spack/repos/builtin/packages/albany/package.py
+++ var/spack/repos/builtin/packages/albany/package.py
@@ -1,27 +1,8 @@
-##############################################################################
-# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
-# Produced at the Lawrence Livermore National Laboratory.
+# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
+# Spack Project Developers. See the top-level COPYRIGHT file for details.
 #
-# This file is part of Spack.
-# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
-# LLNL-CODE-647188
-#
-# For details, see https://github.com/spack/spack
-# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU Lesser General Public License (as
-# published by the Free Software Foundation) version 2.1, February 1999.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
-# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
-# conditions of the GNU Lesser General Public License for more details.
-#
-# You should have received a copy of the GNU Lesser General Public
-# License along with this program; if not, write to the Free Software
-# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-##############################################################################
+# SPDX-License-Identifier: (Apache-2.0 OR MIT)
+
 from spack import *
 
 
@@ -33,8 +14,8 @@ class Albany(CMakePackage):
        including fluid mechanics, solid mechanics (elasticity and plasticity),
        ice-sheet flow, quantum device modeling, and many other applications."""
 
-    homepage = "http://gahansen.github.io/Albany"
-    git      = "https://github.com/gahansen/Albany.git"
+    homepage = "http://SNLComputation.github.io/Albany"
+    git      = "https://github.com/SNLComputation/Albany.git"
 
     maintainers = ['gahansen']
 
@@ -44,10 +25,6 @@ class Albany(CMakePackage):
             description='Enable LCM')
     variant('aeras',          default=False,
             description='Enable AERAS')
-    variant('qcad',          default=False,
-            description='Enable QCAD')
-    variant('hydride',          default=False,
-            description='Enable HYDRIDE')
     variant('lcm_spec',          default=False,
             description='Enable LCM_SPECULATIVE')
     variant('lame',          default=False,
@@ -58,14 +35,10 @@ class Albany(CMakePackage):
             description='Enable CHECK_FPE')
     variant('scorec',          default=False,
             description='Enable SCOREC')
-    variant('felix',          default=False,
-            description='Enable FELIX')
-    variant('mor',          default=False,
-            description='Enable MOR')
+    variant('ali',          default=False,
+            description='Enable ALI')
     variant('confgui',          default=False,
             description='Enable Albany configuration (CI) GUI')
-    variant('ascr',          default=False,
-            description='Enable ALBANY_ASCR')
     variant('perf',          default=False,
             description='Enable PERFORMANCE_TESTS')
     variant('64bit',          default=True,
@@ -73,7 +46,7 @@ class Albany(CMakePackage):
 
     # Add dependencies
     depends_on('mpi')
-    depends_on('trilinos~superlu-dist+isorropia+tempus+rythmos+teko+intrepid+intrepid2+minitensor+phalanx+pnetcdf+nox+piro+rol+shards+stk+superlu@master,develop')
+    depends_on('trilinos@master,develop~superlu-dist+isorropia+tempus+rythmos+teko+intrepid+intrepid2+minitensor+phalanx+pnetcdf+nox+piro+rol+shards+stk+superlu gotype=long_long')
 
     def cmake_args(self):
         spec = self.spec
@@ -90,10 +63,6 @@ class Albany(CMakePackage):
                            'ON' if '+lcm' in spec else 'OFF'),
                        '-DENABLE_AERAS:BOOL=%s' % (
                            'ON' if '+aeras' in spec else 'OFF'),
-                       '-DENABLE_QCAD:BOOL=%s' % (
-                           'ON' if '+qcad' in spec else 'OFF'),
-                       '-DENABLE_HYDRIDE:BOOL=%s' % (
-                           'ON' if '+hydride' in spec else 'OFF'),
                        '-DENABLE_LCM_SPECULATIVE:BOOL=%s' % (
                            'ON' if '+lcm_spec' in spec else 'OFF'),
                        '-DENABLE_LAME:BOOL=%s' % (
@@ -104,14 +73,10 @@ class Albany(CMakePackage):
                            'ON' if '+fpe' in spec else 'OFF'),
                        '-DENABLE_SCOREC:BOOL=%s' % (
                            'ON' if '+scorec' in spec else 'OFF'),
-                       '-DENABLE_FELIX:BOOL=%s' % (
-                           'ON' if '+felix' in spec else 'OFF'),
-                       '-DENABLE_MOR:BOOL=%s' % (
-                           'ON' if '+mor' in spec else 'OFF'),
+                       '-DENABLE_ALI:BOOL=%s' % (
+                           'ON' if '+ali' in spec else 'OFF'),
                        '-DENABLE_ALBANY_CI:BOOL=%s' % (
                            'ON' if '+ci' in spec else 'OFF'),
-                       '-DENABLE_ASCR:BOOL=%s' % (
-                           'ON' if '+ascr' in spec else 'OFF'),
                        '-DENABLE_PERFORMANCE_TESTS:BOOL=%s' % (
                            'ON' if '+perf' in spec else 'OFF'),
                        '-DENABLE_64BIT_INT:BOOL=%s' % (
