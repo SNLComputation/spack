665af05b1b0ae4c03c152f2e5eca4ef5d5734f50
Irina K. Tezaur <ikalash@sandia.gov>
2020-05-10 14:25:21 -0700

    Fixing m4 installation for Albany.

--- var/spack/repos/builtin/packages/m4/package.py
+++ var/spack/repos/builtin/packages/m4/package.py
@@ -1,8 +1,27 @@
-# Copyright 2013-2019 Lawrence Livermore National Security, LLC and other
-# Spack Project Developers. See the top-level COPYRIGHT file for details.
+##############################################################################
+# Copyright (c) 2013-2018, Lawrence Livermore National Security, LLC.
+# Produced at the Lawrence Livermore National Laboratory.
 #
-# SPDX-License-Identifier: (Apache-2.0 OR MIT)
-
+# This file is part of Spack.
+# Created by Todd Gamblin, tgamblin@llnl.gov, All rights reserved.
+# LLNL-CODE-647188
+#
+# For details, see https://github.com/spack/spack
+# Please also see the NOTICE and LICENSE files for our notice and the LGPL.
+#
+# This program is free software; you can redistribute it and/or modify
+# it under the terms of the GNU Lesser General Public License (as
+# published by the Free Software Foundation) version 2.1, February 1999.
+#
+# This program is distributed in the hope that it will be useful, but
+# WITHOUT ANY WARRANTY; without even the IMPLIED WARRANTY OF
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the terms and
+# conditions of the GNU Lesser General Public License for more details.
+#
+# You should have received a copy of the GNU Lesser General Public
+# License along with this program; if not, write to the Free Software
+# Foundation, Inc., 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
+##############################################################################
 from spack import *
 
 
@@ -12,18 +31,16 @@ class M4(AutotoolsPackage):
     homepage = "https://www.gnu.org/software/m4/m4.html"
     url      = "https://ftpmirror.gnu.org/m4/m4-1.4.18.tar.gz"
 
-    version('1.4.18', sha256='ab2633921a5cd38e48797bf5521ad259bdc4b979078034a3b790d7fec5493fab')
-    version('1.4.17', sha256='3ce725133ee552b8b4baca7837fb772940b25e81b2a9dc92537aeaf733538c9e')
+    version('1.4.18', 'a077779db287adf4e12a035029002d28')
+    version('1.4.17', 'a5e9954b1dae036762f7b13673a2cf76')
 
     patch('gnulib-pgi.patch', when='@1.4.18')
     patch('pgi.patch', when='@1.4.17')
     # from: https://github.com/Homebrew/homebrew-core/blob/master/Formula/m4.rb
     # Patch credit to Jeremy Huddleston Sequoia <jeremyhu@apple.com>
-    patch('secure_snprintf.patch', when='os = highsierra')
-    patch('secure_snprintf.patch', when='os = mojave')
-    patch('secure_snprintf.patch', when='os = catalina')
-    # https://bugzilla.redhat.com/show_bug.cgi?id=1573342
+    patch('secure_snprintf.patch', when='platform_os = highsierra')
     patch('https://src.fedoraproject.org/rpms/m4/raw/5d147168d4b93f38a4833f5dd1d650ad88af5a8a/f/m4-1.4.18-glibc-change-work-around.patch', sha256='fc9b61654a3ba1a8d6cd78ce087e7c96366c290bc8d2c299f09828d793b853c8', when='@1.4.18')
+
 
     variant('sigsegv', default=True,
             description="Build the libsigsegv dependency")
@@ -37,13 +54,7 @@ class M4(AutotoolsPackage):
         args = ['--enable-c++']
 
         if spec.satisfies('%clang') and not spec.satisfies('platform=darwin'):
-            args.append('LDFLAGS=-rtlib=compiler-rt')
-
-        if spec.satisfies('%arm') and not spec.satisfies('platform=darwin'):
-            args.append('LDFLAGS=-rtlib=compiler-rt')
-
-        if spec.satisfies('%fj') and not spec.satisfies('platform=darwin'):
-            args.append('LDFLAGS=-rtlib=compiler-rt')
+            args.append('CFLAGS=-rtlib=compiler-rt')
 
         if spec.satisfies('%intel'):
             args.append('CFLAGS=-no-gcc')
@@ -56,7 +67,7 @@ class M4(AutotoolsPackage):
 
         # http://lists.gnu.org/archive/html/bug-m4/2016-09/msg00002.html
         arch = spec.architecture
-        if (arch.platform == 'darwin' and arch.os == 'sierra' and
+        if (arch.platform == 'darwin' and arch.platform_os == 'sierra' and
             '%gcc' in spec):
             args.append('ac_cv_type_struct_sched_param=yes')
 
